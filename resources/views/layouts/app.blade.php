1359924d162273176fefdd39dbc2ec1565f877b7
Luke Joseph <luke@trackwire.io>
2021-06-08 16:44:27 +0200

    install and pull in tailwind

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+@include('layouts.head')
+    <body class="border-t-4 border-blue-400">
+
+      @include('layouts.nav')
+
+      <div class="container mx-auto">
+
+        @yield('content')
+
+      </div>
+
+    </body>
+</html>
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,12 @@
+<nav class="h-16 w-full bg-black">
+  <div class="nav-items flex container mx-auto border-blue-800 border h-full justify-between">
+    <div class="flex items-center w-1/3 text-lg grid grid-flow-col grid-cols-3">
+      <div class="text-white">Popular </div>
+      <div class="text-white">New Releases </div>
+      <div class="text-white">Highest Rating </div>
+    </div>
+    <div class="w-1/3">
+
+    </div>
+  </div>
+</nav>
