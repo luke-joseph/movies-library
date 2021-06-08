808b1efaab0df05bcd6e2f4743a3e54efb349d1d
Luke Joseph <luke@trackwire.io>
2021-06-08 21:41:38 +0200

    make a test db call

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Http;
 
 Route::get('/', function () {
     return view('index');
@@ -9,4 +9,12 @@ Route::get('/', function () {
 
 Route::get('/show', function () {
     return view('show');
+});
+
+Route::get('/test-tmdb', function () {
+    // make request
+    $response = Http::withToken(config('services.tmdb.key'))
+    ->get('https://api.themoviedb.org/3/movie/632357');
+
+    dd($response->json());
 });
