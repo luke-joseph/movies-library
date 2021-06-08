78ca6a9ca71e458bec5cd4fbe4eead9a918c3d0c
Luke Joseph <luke@trackwire.io>
2021-06-08 18:47:18 +0200

    give more space to the layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1 class="text-white text-6xl font-thin">Movies DB</h1>
-
   {{-- popular movies --}}
 
-  <div class="mt-8">
-    <h2 class="text-3xl font-bold text-indigo-400">Popular Movies</h2>
+  <div class="mt-16">
+    <h2 class="text-3xl font-bold text-indigo-300">// Popular Movies</h2>
 
-    <div class="movie-poster-container mt-8 grid grid-flow-col grid-cols-4 grid-rows-2 gap-x-4 gap-y-10">
+    <div class="movie-poster-container mt-12 grid grid-flow-col grid-cols-4 grid-rows-2 gap-x-4 gap-y-16">
+
       <div class="">
         <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg" alt="">
         <h3 class="text-3xl mt-2 tracking-wider">Movie Title</h3>
@@ -70,14 +69,16 @@
 
   {{-- Todays Recommended --}}
 
-  <div class="mt-8">
-    <h2 class="text-3xl">Today's Recommended Movie</h2>
+  <div class="mt-16">
+    <h2 class="text-3xl font-bold text-indigo-300">// Today's Recommended Movie</h2>
 
     <div class="recommended-movie-container mt-8 grid grid-flow-col grid-cols-4 gap-4">
+
       <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg" alt="">
       <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg" alt="">
       <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg" alt="">
       <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg" alt="">
+
     </div>
   </div>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,16 @@
 @include('layouts.head')
-    <body class="border-t-4 border-indigo-600 bg-gray-800 text-gray-300 mb-16">
+    <body class="border-t-4 border-indigo-600 bg-gray-800 text-gray-300">
 
       @include('layouts.nav')
 
-      <div class="container mx-auto mt-16">
+      <div class="container mx-auto mt-16 mb-16">
 
         @yield('content')
 
       </div>
 
+      <div class="h-16 bg-black">
+
+      </div>
     </body>
 </html>
